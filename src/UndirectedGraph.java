7cb8700ff5a5a9988436bc81fcedf16b8dad8a3f
Tyler Laskey <tlaskey3@gmail.com>
2017-05-11 22:31:57 -0700

    setting up graph

--- src/UndirectedGraph.java
+++ src/UndirectedGraph.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
 
 public class UndirectedGraph
 {
+    private ArrayList<Node> nodes;
 
+    public UndirectedGraph(int n)
+    {
+        nodes = createRandomGraph(n);
+    }
+
+    public static ArrayList<Node> createRandomGraph(int n)
+    {
+        return null;
+    }
+
+    public static ArrayList<Node> setAdjacencyList()
+    {
+        return null;
+    }
+
+    public int findMaxClique()
+    {
+        return 0;
+    }
+}
+
+class Node
+{
+    private ArrayList<Node> adjacencyList;
+    private int n;
+
+    public Node(int n)
+    {
+        adjacencyList = new ArrayList<Node>();
+        this.n = n;
+    }
 }
