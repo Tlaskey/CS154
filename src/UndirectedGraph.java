2c2dd2ff06c57a7b6dbfc7071318cab6893cbe60
Tyler Laskey <tlaskey3@gmail.com>
2017-05-13 20:00:48 -0700

    Create random graph2

--- src/UndirectedGraph.java
+++ src/UndirectedGraph.java
@@ -35,19 +35,19 @@ public class UndirectedGraph
 
     public void createRandomGraph(int n)
     {
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             addNode(i);
         }
         Random rand = new Random();
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             int numEdges = rand.nextInt(n / 2) + 1;
             ArrayList<Integer> adjList = nodes.get(i);
             for (int j = 1; j <= numEdges; j++)
             {
                 int random = rand.nextInt(n) + 1;
-                if (!adjList.contains(random))
+                if (!adjList.contains(random) && random != i)
                 {
                     adjList.add(random);
                 }
@@ -57,7 +57,7 @@ public class UndirectedGraph
 
     public void printGraph()
     {
-        for (int i = 1; i < numNodes; i++)
+        for (int i = 1; i <= numNodes; i++)
         {
             ArrayList<Integer> adjList = nodes.get(i);
             System.out.println("Node: " + i);
