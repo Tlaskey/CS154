75e7715cc84a5048c93b2137b0e0dee6f84aa759
Tyler Laskey <tlaskey3@gmail.com>
2017-05-15 22:02:52 -0700

    testing

--- src/UndirectedGraph.java
+++ src/UndirectedGraph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -24,6 +25,15 @@ public class UndirectedGraph
     }
 
     /**
+     * UndirectedGraph constructor for empty graph.
+     */
+    public UndirectedGraph()
+    {
+        nodes = new ArrayList<>();
+        numNodes = 0;
+    }
+
+    /**
      * Add node to graph.
      * 
      * @param newNode
@@ -32,6 +42,33 @@ public class UndirectedGraph
     public void addNode(int newNode)
     {
         nodes.add(new Node(newNode));
+        numNodes++;
+    }
+
+    /**
+     * Adds a specific node to graph.
+     * 
+     * @param node
+     *            the node to be added.
+     */
+    public void addNode(Node node)
+    {
+        nodes.add(node);
+        numNodes++;
+    }
+
+    /**
+     * populates the graph with n nodes.
+     * 
+     * @param n
+     *            number of nodes to add.
+     */
+    public void populateGraph(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            addNode(i);
+        }
     }
 
     /**
@@ -43,10 +80,7 @@ public class UndirectedGraph
     public void createRandomGraph(int n)
     {
         // Populate graph with n nodes.
-        for (int i = 1; i <= n; i++)
-        {
-            addNode(i);
-        }
+        populateGraph(n);
         // Randomize number of edges for each node.
         Random rand = new Random();
         int numEdges = rand.nextInt(n / 2) + 1;
@@ -144,12 +178,98 @@ public class UndirectedGraph
         }
     }
 
+    /**
+     * Checks for cliques in each subgraph.
+     * 
+     * @param subGraphs
+     */
+    public static void isClique(ArrayList<ArrayList<Node>> subGraphs)
+    {
+        for (ArrayList<Node> subGraph : subGraphs)
+        {
+            System.out.println(isSubGraphClique(subGraph));
+        }
+    }
+
+    /**
+     * Checks if the subGraph is a clique.
+     * 
+     * @param subGraph
+     *            the subgraph to check.
+     * @return true or false
+     */
+    public static boolean isSubGraphClique(ArrayList<Node> subGraph)
+    {
+        boolean isClique = true;
+        ArrayList<Node> clique = new ArrayList<>();
+        for (Node n : subGraph)
+        {
+            clique.add(n);
+            for (Node cliqueMembers : clique)
+            {
+                if (cliqueMembers.equals(n))
+                {
+                } else if (!cliqueMembers.getAdjList().contains(n))
+                {
+                    isClique = false;
+                }
+            }
+        }
+        return isClique;
+    }
+
+    /**
+     * gets the nodes of graph.
+     * 
+     * @return Arraylist of nodes.
+     */
+    public ArrayList<Node> getNodes()
+    {
+        return nodes;
+    }
+
     public static void main(String[] args)
     {
-        UndirectedGraph g = new UndirectedGraph(12);
-        g.printGraph();
-        ArrayList<ArrayList<Node>> divided = g.divideGraph(5);
-        printSubGraphs(divided);
+        UndirectedGraph test = new UndirectedGraph();
+        Node one = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        Node five = new Node(5);
+        Node six = new Node(6);
+        Node seven = new Node(7);
+        Node eight = new Node(8);
+        ArrayList<Node> oneList = new ArrayList<>(Arrays.asList(four, six, seven, two));
+        ArrayList<Node> twoList = new ArrayList<>(Arrays.asList(seven, eight, four, six, one));
+        ArrayList<Node> threeList = new ArrayList<>(Arrays.asList(five, six));
+        ArrayList<Node> fourList = new ArrayList<>(Arrays.asList(one, six, seven, five, two));
+        ArrayList<Node> fiveList = new ArrayList<>(Arrays.asList(four, three));
+        ArrayList<Node> sixList = new ArrayList<>(Arrays.asList(one, four, three, seven, two));
+        ArrayList<Node> sevenList = new ArrayList<>(Arrays.asList(two, four, six));
+        ArrayList<Node> eightList = new ArrayList<>(Arrays.asList(two));
+        one.setAdjList(oneList);
+        two.setAdjList(twoList);
+        three.setAdjList(threeList);
+        four.setAdjList(fourList);
+        five.setAdjList(fiveList);
+        six.setAdjList(sixList);
+        seven.setAdjList(sevenList);
+        eight.setAdjList(eightList);
+        test.addNode(one);
+        test.addNode(two);
+        test.addNode(three);
+        test.addNode(four);
+        test.addNode(five);
+        test.addNode(six);
+        test.addNode(seven);
+        test.addNode(eight);
+        ArrayList<ArrayList<Node>> testDivide = new ArrayList<>();
+        ArrayList<Node> list1 = new ArrayList<>(Arrays.asList(one, four, six, seven, two));
+        ArrayList<Node> list2 = new ArrayList<>(Arrays.asList(five, eight, three));
+        testDivide.add(list1);
+        testDivide.add(list2);
+        printSubGraphs(testDivide);
+        isClique(testDivide);
     }
 }
 
@@ -178,6 +298,11 @@ class Node
     public ArrayList<Node> getAdjList()
     {
         return adjList;
+    }
+
+    public void setAdjList(ArrayList<Node> adj)
+    {
+        this.adjList = adj;
     }
 
     public int nodeDegree()
